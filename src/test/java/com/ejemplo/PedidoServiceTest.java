0405e1c81f29b4415a175e0ad8aec4d0368ee6de
Nicolas Gravertt <nicolasgravertt@hotmail.com>
2025-06-23 22:36:18 -0400

    Add initial unit tests template for PedidoService without mocking

---
+++ src/test/java/com/ejemplo/PedidoServiceTest.java
@@ -0,0 +1,28 @@
+// Como seria sin mocks
+package com.ejemplo;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+
+class PedidoServiceTest {
+//    PedidoService service = new PedidoService();
+//    @Test
+//    void testSinDescuentoYEnvioNormal() {
+//        double total = service.calcularTotal(100, false, false);
+//        assertEquals(110.0, total);
+//    }
+//    @Test
+//    void testConDescuentoYEnvioExpress() {
+//        double total = service.calcularTotal(100, true, true);
+//        assertEquals(110.0, total); // 100 - 10% + 20
+//    }
+//    @Test
+//    void testConDescuentoYEnvioNormal() {
+//        double total = service.calcularTotal(200, true, false);
+//        assertEquals(190.0, total);
+//    }
+//    @Test
+//    void testSinDescuentoYEnvioExpress() {
+//        double total = service.calcularTotal(150, false, true);
+//        assertEquals(170.0, total);
+//    }
+}
